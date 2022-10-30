730646ccfd5921439298883e2bc01581ff42918d
Amir Feizi <amir.feizi@omass.com>
2022-10-30 19:58:41 +0000

    added the core test files for all functions

--- tests/testthat.R
+++ tests/testthat.R
@@ -2,3 +2,6 @@ library(testthat)
 library(otargen)
 
 test_check("otargen")
+
+# to be removed when publishing the package
+#sapply(stringr::str_replace(list.files("./R/"),".R$",""),usethis::use_test)
