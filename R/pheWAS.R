496d835d189169d3918eccf59d30e0b2d621dceb
Kamalika Ray <kamalika.ray@compchem2.adds.omass.com>
2023-01-31 12:02:25 +0000

    pheWAS

--- R/pheWAS.R
+++ R/pheWAS.R
@@ -9,9 +9,44 @@
 #'
 
 pheWAS <- function(variantid) {
+
+  # make connection to the endpoint
+
+  cli::cli_progress_step("Connecting the database...", spinner = TRUE)
+  client <- ghql::GraphqlClient$new(url = "https://api.genetics.opentargets.org/graphql")
+  query_class <- ghql::Query$new()
+
+  # Check variant id format
+  if (grepl(pattern = "rs\\d+", variantid)) {
+
+    # Convert rs id to variant id
+    query_searchid <- "query ConvertRSIDtoVID($queryString:String!) {
+    search(queryString:$queryString){
+      totalVariants
+      variants{
+        id
+        }
+      }
+    }"
+
+    variables <- list(queryString = variantid)
+    query_class$query(name = "convertid", x = query_searchid)
+    id_result <- jsonlite::fromJSON(client$exec(query_class$queries$convertid, variables), flatten=TRUE)$data
+    input_variantid <- id_result$search$variants$id
+  }
+
+  else if (grepl(pattern = "\\d+_\\d+_[a-zA-Z]+_[a-zA-Z]+", variantid))
+  {
+    input_variantid <- variantid
+  }
+  else
+  {
+    stop("\n Please provide a variant Id")
+  }
+
+
   ## Set up to query Open Targets Genetics API
 
-  variables <- list(variantId = variantid)
   query <- 'query search($variantId: String!){
     pheWAS(variantId: $variantId){
     totalGWASStudies
@@ -32,13 +67,12 @@ pheWAS <- function(variantid) {
   }
 }'
 
-  # make connection to the endpoint
 
-  cli::cli_progress_step("Connecting the database...", spinner = TRUE)
-  client <- ghql::GraphqlClient$new(url = "https://api.genetics.opentargets.org/graphql")
-  query_class <- ghql::Query$new()
 
   # execute the query
+
+  variables <- list(variantId = input_variantid)
+
   query_class$query(name ='phewas_query' , x = query )
 
   cli::cli_progress_step(paste("Downloading data...",variantid,"..."), spinner = TRUE)
