1c8a023030de1d9373c9c14b6a1ae7a0054470be
Kamalika Ray <kamalika.ray@compchem2.adds.omass.com>
2023-02-07 07:27:13 +0000

    Plot l2g update

--- R/plot_l2g.R
+++ R/plot_l2g.R
@@ -16,42 +16,31 @@ plot_l2g <- function(data, disease_efo=NULL){
   exclude <- c("phenotype", "measurement", "Uncategorised", "biological process")
   data <- dplyr::filter(data, !study.traitCategory %in% exclude)
 
-  df <- data[,c("yProbaModel","yProbaDistance","yProbaInteraction","yProbaMolecularQTL", "yProbaPathogenicity", "pval",
-                "study.traitReported", "study.traitEfos", "study.traitCategory","gene_symbol")]
+  df <- data[,c("yProbaModel","yProbaDistance","yProbaInteraction","yProbaPathogenicity", "yProbaMolecularQTL","gene_symbol",
+                "study.traitReported", "study.traitEfos", "study.traitCategory","pval")]
 
-  df <- setNames(df, c("L2G_score","Distance","Interaction", "Pathogenicity", "mQTL", "pval",
-                       "Traits", "EFO_ID","Trait_category", "Gene_name"))
+  df <- setNames(df, c("L2G_score","Distance","Interaction", "Pathogenicity", "mQTL", "Gene_name", "Traits",
+                       "EFO_ID","Trait_category", "pval"))
 
   df <- df[order(df$L2G_score,decreasing=TRUE),]
 
 
   if (!is.null(disease_efo)){
     df <- df %>% dplyr::filter(EFO_ID == disease_efo) %>% dplyr::group_by(Gene_name) %>% dplyr::filter(L2G_score == max(L2G_score)) %>% data.frame()
-    loop_num <- 1
+    names <- df$Gene_name
+    rownames(df) <- names
+    df <- tibble::rownames_to_column(df, "group")
+    ggradar::ggradar(df[,1:6], values.radar = c(0, 0.5, 1), group.line.width = 1,background.circle.colour = "grey",
+                     group.point.size = 2, legend.position = "bottom", plot.title=df[1,'Traits'])
   }
   else{
     df <- df %>% dplyr::group_by(Gene_name) %>% dplyr::filter(L2G_score == max(L2G_score)) %>% data.frame()
-    loop_num <- length(unique(as.list(df$EFO_ID)))
-  }
-
-  names <- df$Gene_name
-  rownames(df) <- names
-  df <- tibble::rownames_to_column(df, "group")
-
-  if (loop_num == 1){
-    trait_name=df[1,'Traits']
-    ggradar::ggradar(df[,1:6], values.radar = c(0, 0.5, 1), group.line.width = 1,
-                     group.point.size = 2, legend.position = "bottom", plot.title=trait_name)
-  }
-  else{
-  plots <- list()
-  for (i in 1:loop_num){
-  trait_name=df[i,'Traits']
-  plots[[i]]=ggradar::ggradar(df[i,1:6], values.radar = c(0, 0.5, 1), group.line.width = 1,
-                   group.point.size = 2, legend.position = "bottom", plot.title=trait_name)
-  }
-  print (df['group'])
-  plots
+    names <- df$Traits
+    rownames(df) <- names
+    df <- tibble::rownames_to_column(df, "group")
+    df <- df %>% dplyr::mutate(group = paste(group, Gene_name, sep=": "))
+    ggradar::ggradar(df[,1:6], values.radar = c(0, 0.5, 1), group.line.width = 1,background.circle.colour = "grey",
+                     group.point.size = 2, legend.position = "bottom") + ggplot2::facet_wrap(~group) + ggplot2::theme_light()
   }
 
 }
