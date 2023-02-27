f82a0ebaa1180942db770487354b0469365fc165
Kamalika Ray <kamalika.ray@compchem2.adds.omass.com>
2023-02-27 17:39:42 +0000

    Manhattan plot

--- R/plot_manhattan.R
+++ R/plot_manhattan.R
@@ -8,30 +8,32 @@
 #' @export
 #'
 #' @examples
-#' manhattan(studyid = "GCST90002357") %>% plot_l2g(pval= 10e-8)
+#' manhattan(studyid = "GCST003044") %>% plot_l2g(pval= 10e-8)
 #'
 
-plot_l2g <- function(data, pval=10e-8){
-  data %>% filter(pval <= 10e-8)
-  gwas_data <- data[c('variant.id', 'variant.position', 'variant.chromosome', 'pval')]
-  manhplot <- ggplot(gwas_data, aes(x = variant.position, y = -log10(pval),
-                                    color = as_factor(variant.chromosome), size = -log10(pval))) +
-    geom_hline(yintercept = -log10(sig), color = "grey40", linetype = "dashed") +
-    geom_point(alpha = 0.75) +
-    scale_x_continuous(label = axis_set$chr, breaks = axis_set$center) +
-    scale_y_continuous(expand = c(0,0), limits = c(0, ylim)) +
-    scale_color_manual(values = rep(c("#276FBF", "#183059"), unique(length(axis_set$chr)))) +
-    scale_size_continuous(range = c(0.5,3)) +
-    labs(x = NULL,
-         y = "-log<sub>10</sub>(p)") +
-    theme_minimal() +
-    theme(
-      legend.position = "none",
-      panel.grid.major.x = element_blank(),
-      panel.grid.minor.x = element_blank(),
-      axis.title.y = element_markdown(),
-      axis.text.x = element_text(angle = 60, size = 8, vjust = 0.5)
-    )
+plot_l2g <- function(data, pvalue=10e-8){
+  data %>% filter(pval <= pvalue)
+  gwasResults <- data[c('variant.position', 'variant.chromosome', 'pval', 'variant.id')] %>% unique()
+  gwasResults <- gwasResults %>%  dplyr::rename("Positions"="variant.position", "CHR"="variant.chromosome", "P"="pval", "SNP"="variant.id")
+  gwasResults$text <- paste("SNP: ", gwasResults$SNP, "\nPosition: ", gwasResults$Positions, "\nChromosome: ", gwasResults$CHR, "\nLOD score:", -log10(gwasResults$P) %>% round(2), sep="")
+  print (gwasResults)
+  axisdf <- gwasResults %>% dplyr::group_by(CHR) %>% dplyr::summarize(center=( max(Positions) + min(Positions) ) / 2 )
+  # Make the plot
+  p <- ggplot2::ggplot(gwasResults, ggplot2::aes(x=Positions, y=-log10(P), text=text)) +
 
+    # Show all points
+    ggplot2::geom_point(ggplot2::aes(color=CHR)) +
+    ggplot2::scale_color_manual(values = rep(c("darkblue", "darkgreen"))) +
+
+    # custom X axis:
+    ggplot2::scale_x_continuous(label = axisdf$CHR, breaks= axisdf$center) +
+    ggplot2::scale_y_continuous(expand = c(0, 0)) +
+
+    # Custom the theme:
+    ggplot2::theme_bw() +
+    ggplot2::theme(legend.position="right", panel.border = ggplot2::element_blank(), panel.grid.major.x = ggplot2::element_blank(),
+      panel.grid.minor.x = ggplot2::element_blank())
+
+  plotly::ggplotly(p, tooltip="text")
 
 }
