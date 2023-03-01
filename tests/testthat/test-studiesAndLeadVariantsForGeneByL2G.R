871554aa4ce00e21b783cbcf85acc1039aca56d1
Kamalika Ray <kamalika.ray@compchem2.adds.omass.com>
2023-03-01 11:28:21 +0000

    manhattan plot and test

--- tests/testthat/test-studiesAndLeadVariantsForGeneByL2G.R
+++ tests/testthat/test-studiesAndLeadVariantsForGeneByL2G.R
@@ -1,4 +1,4 @@
-test_that("multiplication works", {
+test_that("test studiesAndLeadVariantsForGeneByL2G", {
   expected <- otargen::studiesAndLeadVariantsForGeneByL2G(ensmbl_ids = "ENSG00000169174")
   expect_s3_class(expected, "data.frame")
   expect_error(otargen::studiesAndLeadVariantsForGeneByL2G(ensmbl_ids = "ENSGXXXXXXXXX"))
--- tests/testthat/test-topOverlappedStudies.R
+++ tests/testthat/test-topOverlappedStudies.R
@@ -1,3 +1,5 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+test_that("test the topOverlappedStudies", {
+  expected <-  otargen::manhattan(studyid = "NEALE2_6177_1", pageindex = 1, pagesize = 50)
+  expect_s3_class(expected, "data.frame")
+  expect_false(is.null(dim(expected)))
 })
