5a1f4fd4ce6b099a3c41f0a6334ecef5c07f017d
Amir Feizi <amir.feizi@omass.com>
2023-07-19 13:57:32 +0100

    fixed a bug in variantInfo function for null data

--- R/variantInfo.R
+++ R/variantInfo.R
@@ -92,6 +92,7 @@ variantInfo <- function(variantid) {
   otg_qry$query(name = "variantInfoquery", x =  query)
 
   cli::cli_progress_step("Downloading data...", spinner = TRUE)
+  result <- jsonlite::fromJSON(otg_cli$exec(otg_qry$queries$variantInfoquery, variables), flatten = TRUE)$data
   result <- lapply(result$variantInfo, function(x) if (is.null(x)) "NA" else x)
   result <- as.data.frame(result)
   return (result)
